8b8ce2f493f697aa38d51be2f4762c20e31eb097
Pamela Chawira <125671453+Pam-123@users.noreply.github.com>
2024-03-22 09:47:30 +0200

    Quickcheck

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""A base models class"""
+"""
+    BaseModel module
+"""
 
 import uuid
 from datetime import datetime
@@ -7,45 +9,40 @@ import models
 
 
 class BaseModel:
-    """The base model class for other classes"""
-
     def __init__(self, *args, **kwargs):
-        """Initialise public instance attributes"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            kwargs["created_at"] = datetime.strptime(
-                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
-            )
-            kwargs["updated_at"] = datetime.strptime(
-                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
-            )
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
             for key, value in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, value)
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime
+                                .strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        models.storage.new(self)
 
     def __str__(self):
-        """String representation of class"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """
+        Returns string representation of the BaseModel instance.
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
         """
-        updates the public instance attribute `updated_at`
-        with the current datetime
+        Updates updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values
-          of __dict__ of the instance.
+        Returns a dict containing all keys/values of __dict__ of the instance.
         """
-        dictionary = self.__dict__.copy()
-        dictionary["__class__"] = self.__class__.__name__
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
-        return dictionary
+        obj_instance = self.__dict__.copy()
+        obj_instance['__class__'] = self.__class__.__name__
+        obj_instance['created_at'] = self.created_at.isoformat()
+        obj_instance['updated_at'] = self.updated_at.isoformat()
+        return obj_instance
