96464f5ea26df64ba006706de50bc290eac6ea1c
VictorNalu <victorchinalurumogu@gmail.com>
2024-03-22 19:07:31 +0100

    test_2

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,46 +1,44 @@
 #!/usr/bin/python3
-"""Testing the file_storage module"""
+"""Unit tests for FileStorage class"""
 
 import unittest
 import os
-import time
-import json
 from models.base_model import BaseModel
+from models.user import User
 from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
-    """Test suite for the FileStorage class."""
+    """Test suite for FileStorage class."""
 
     def setUp(self):
         """Set up the test environment."""
-        # Create a FileStorage instance for each test
         self.storage = FileStorage()
 
     def tearDown(self):
         """Clean up the test environment."""
-        # Remove the file created during the tests, if it exists
         if os.path.exists(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
     def test_save_and_reload(self):
         """Test saving and reloading objects using FileStorage."""
-        # Create two BaseModel instances
         model1 = BaseModel()
         model2 = BaseModel()
-
-        # Add the BaseModel instances to the FileStorage and save
+        user1 = User()
+        user2 = User()
         self.storage.new(model1)
         self.storage.new(model2)
+        self.storage.new(user1)
+        self.storage.new(user2)
         self.storage.save()
 
-        # Clear the FileStorage and reload from the file
-        self.storage._FileStorage_objects = {}
+        self.storage._FileStorage__objects = {}
         self.storage.reload()
 
-        # Verify that the objects were reloaded correctly
         self.assertIn("BaseModel." + model1.id, self.storage.all())
         self.assertIn("BaseModel." + model2.id, self.storage.all())
+        self.assertIn("User." + user1.id, self.storage.all())
+        self.assertIn("User." + user2.id, self.storage.all())
 
 
 if __name__ == "__main__":
