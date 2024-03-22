a429a94ad9ac2a83d17fd0c446d3b7aa8d8fa231
VictorNalu <victorchinalurumogu@gmail.com>
2024-03-22 07:41:05 +0100

    test_1

--- models/base_model.py
+++ models/base_model.py
@@ -3,24 +3,29 @@
 
 import uuid
 from datetime import datetime
-from . import storage
-
+import models
 
 
 class BaseModel:
     """The base model class for other classes"""
 
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                setattr(self, key, value)
-        else:
+        """Initialise public instance attributes"""
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            kwargs["created_at"] = datetime.strptime(
+                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            kwargs["updated_at"] = datetime.strptime(
+                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+            )
+            for key, value in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, value)
 
     def __str__(self):
         """String representation of class"""
@@ -32,7 +37,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
