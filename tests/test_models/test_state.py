5d926cfe07923f09148ff8e60d0ee7782a5db4a7
VictorNalu <victorchinalurumogu@gmail.com>
2024-03-23 20:10:08 +0100

    test_3

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,65 @@
+import unittest
+from models.state import State
+from datetime import datetime
+
+
+class TestState(unittest.TestCase):
+    """
+    Test cases for the State class.
+    """
+
+    def setUp(self):
+        """
+        Set up a State instance before each test.
+        """
+        self.state = State()
+
+    def test_attributes(self):
+        """
+        Test the existence of attributes.
+        """
+        self.assertTrue(hasattr(self.state, "id"))
+        self.assertTrue(hasattr(self.state, "created_at"))
+        self.assertTrue(hasattr(self.state, "updated_at"))
+        self.assertTrue(hasattr(self.state, "name"))
+
+    def test_attribute_defaults(self):
+        """
+        Test default attribute values.
+        """
+        self.assertEqual(self.state.name, "")
+
+    def test_created_at_type(self):
+        """
+        Test the type of the 'created_at' attribute.
+        """
+        self.assertIsInstance(self.state.created_at, datetime)
+
+    def test_updated_at_type(self):
+        """
+        Test the type of the 'updated_at' attribute.
+        """
+        self.assertIsInstance(self.state.updated_at, datetime)
+
+    def test_str_representation(self):
+        """
+        Test the string representation of the State instance.
+        """
+        expected_str = "[State] ({}) {}".format(self.state.id, self.state.__dict__)
+        self.assertEqual(str(self.state), expected_str)
+
+    def test_to_dict_method(self):
+        """
+        Test the to_dict method of the State class.
+        """
+        obj_dict = self.state.to_dict()
+        self.assertTrue(isinstance(obj_dict, dict))
+        self.assertEqual(obj_dict["__class__"], "State")
+        self.assertTrue("id" in obj_dict)
+        self.assertTrue("created_at" in obj_dict)
+        self.assertTrue("updated_at" in obj_dict)
+        self.assertTrue("name" in obj_dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,29 +2,75 @@
 """Unit tests for User class"""
 
 import unittest
-import os
 from models.user import User
+from datetime import datetime
 
 
 class TestUser(unittest.TestCase):
-    """Test suite for User class."""
+    """
+    Test cases for the User class.
+    """
 
-    def test_user_creation(self):
-        """Test User object creation."""
-        user = User()
-        self.assertIsInstance(user, User)
-        self.assertTrue(hasattr(user, "email"))
-        self.assertTrue(hasattr(user, "password"))
-        self.assertTrue(hasattr(user, "first_name"))
-        self.assertTrue(hasattr(user, "last_name"))
+    def setUp(self):
+        """
+        Set up a User instance before each test.
+        """
+        self.user = User()
 
-    def test_user_to_dict(self):
-        """Test User object to_dict method."""
-        user = User()
-        user_dict = user.to_dict()
-        self.assertEqual(user_dict["__class__"], "User")
-        self.assertIsInstance(user_dict["created_at"], str)
-        self.assertIsInstance(user_dict["updated_at"], str)
+    def test_attributes(self):
+        """
+        Test the existence of attributes.
+        """
+        self.assertTrue(hasattr(self.user, "id"))
+        self.assertTrue(hasattr(self.user, "created_at"))
+        self.assertTrue(hasattr(self.user, "updated_at"))
+        self.assertTrue(hasattr(self.user, "email"))
+        self.assertTrue(hasattr(self.user, "password"))
+        self.assertTrue(hasattr(self.user, "first_name"))
+        self.assertTrue(hasattr(self.user, "last_name"))
+
+    def test_attribute_defaults(self):
+        """
+        Test default attribute values.
+        """
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+
+    def test_created_at_type(self):
+        """
+        Test the type of the 'created_at' attribute.
+        """
+        self.assertIsInstance(self.user.created_at, datetime)
+
+    def test_updated_at_type(self):
+        """
+        Test the type of the 'updated_at' attribute.
+        """
+        self.assertIsInstance(self.user.updated_at, datetime)
+
+    def test_str_representation(self):
+        """
+        Test the string representation of the User instance.
+        """
+        expected_str = "[User] ({}) {}".format(self.user.id, self.user.__dict__)
+        self.assertEqual(str(self.user), expected_str)
+
+    def test_to_dict_method(self):
+        """
+        Test the to_dict method of the User class.
+        """
+        obj_dict = self.user.to_dict()
+        self.assertTrue(isinstance(obj_dict, dict))
+        self.assertEqual(obj_dict["__class__"], "User")
+        self.assertTrue("id" in obj_dict)
+        self.assertTrue("created_at" in obj_dict)
+        self.assertTrue("updated_at" in obj_dict)
+        self.assertTrue("email" in obj_dict)
+        self.assertTrue("password" in obj_dict)
+        self.assertTrue("first_name" in obj_dict)
+        self.assertTrue("last_name" in obj_dict)
 
 
 if __name__ == "__main__":
