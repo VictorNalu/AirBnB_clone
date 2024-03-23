f3e24a214025b8ae0919a52418240e3440070bb9
VictorNalu <victorchinalurumogu@gmail.com>
2024-03-23 19:26:04 +0100

    test_model

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""fileStorage """
 import json
 import os
 from models.base_model import BaseModel
@@ -11,19 +12,24 @@ from models.review import Review
 
 
 class FileStorage:
+    """FileStorage class"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """all method"""
         return FileStorage.__objects
 
     def new(self, obj):
+        """new"""
         obj_class_name = obj.__class__.__name__
         obj_key = "{}.{}".format(obj_class_name, obj.id)
 
         FileStorage.__objects[obj_key] = obj
 
     def save(self):
+        """save"""
         obj_dict = {}
         for key, obj in FileStorage.__objects.items():
             obj_dict[key] = obj.to_dict()
@@ -32,12 +38,13 @@ class FileStorage:
             json.dump(obj_dict, file)
 
     def reload(self):
+        """reload"""
         if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
                 try:
                     obj_dict = json.load(file)
                     for key, value in obj_dict.items():
-                        class_name, obj_id = key.split('.')
+                        class_name, obj_id = key.split(".")
                         cls = eval(class_name)
                         inst = cls(**value)
                         FileStorage.__objects[key] = inst
