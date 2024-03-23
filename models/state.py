e3e1a25bfb0b327862305b7cec6a6f14c9e05bfa
VictorNalu <victorchinalurumogu@gmail.com>
2024-03-23 20:16:49 +0100

    test_3_1

--- models/state.py
+++ models/state.py
@@ -6,6 +6,4 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """State class"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = ""
+    name = ""
--- models/user.py
+++ models/user.py
@@ -6,9 +6,7 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User class"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
